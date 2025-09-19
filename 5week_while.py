a87046f9222318934fa39519c3c9b4d9244aba55
msayracode <msayracode@gmail.com>
2025-09-19 14:37:20 +0300

    break and continue are covered

---
+++ 5week_while.py
@@ -0,0 +1,16 @@
+# sayilar = [1,2,3,4,5,6,7,8,9]
+
+# index = 0
+
+# while index < len(sayilar):
+#     print(sayilar[index])
+#     index += 1
+
+name = "Sadık Turan"
+
+for letter in name:
+    if letter == "a":
+        continue
+    print(letter)
+
+
