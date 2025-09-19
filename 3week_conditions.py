1993a3e9f087ac08de5c69d603b9c1432d5f1a5b
msayracode <msayracode@gmail.com>
2025-09-19 10:31:25 +0300

    for loop covered

---
+++ 3week_conditions.py
@@ -0,0 +1,21 @@
+
+x = 20
+y = 20
+
+if x>y:
+    print("x > y")
+elif x == y:
+    print ("x = y")
+else:
+    print("y x den büyük")
+
+
+num = 0
+
+if num > 0:
+    print("pozitif")
+elif num < 0:
+    print("negatif")
+else:
+    print("sıfır")
+
---
+++ 4wee_forloop.py
@@ -0,0 +1,33 @@
+
+# numbers = [ 1,2,3,4,5]
+
+# for number in numbers:
+#     print(number)
+# print("**********************")
+# for i in range(1,10,2):
+#     print(i)
+
+
+# tuple = [(1,2),(3,4),(5,6)]
+
+# for a,b in tuple:
+#     print(a)
+#     print(b)
+
+# d = {"k1": 1, "k2":2, "k3":3}
+
+# for key in d:
+#     print(key)
+
+# for key, value in d.items():
+#     print(value)
+
+sayilar = [1,2,3,4]
+top=0
+for sayi in sayilar:
+    top += sayi
+print(top)
+
+
+
+
