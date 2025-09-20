98ae18a1d1131c0fe44ca935eef9917ee9f49f54
msayracode <msayracode@gmail.com>
2025-09-20 17:46:18 +0300

    range, enumerate, zip, comprehension

--- 5week_while.py
+++ 5week_while.py
@@ -1,25 +1,24 @@
-# sayilar = [1,2,3,4,5,6,7,8,9]
+# # sayilar = [1,2,3,4,5,6,7,8,9]
 
-# index = 0
+# # index = 0
 
-# while index < len(sayilar):
-#     print(sayilar[index])
-#     index += 1
+# # while index < len(sayilar):
+# #     print(sayilar[index])
+# #     index += 1
 
-# name = "Sadık Turan"
+# # name = "Sadık Turan"
 
-# for letter in name:
-#     if letter == "a":
+# # for letter in name:
+# #     if letter == "a":
+# #         continue
+# #     print(letter)
+
+# x = 0
+
+# while x < 5:
+#     x += 1
+#     if x == 2:
 #         continue
-#     print(letter)
-
-x = 0
-
-while x < 5:
-    x += 1
-    if x == 2:
-        continue
-    print(x)
-    x += 1
-
+#     print(x)
+#     x += 1
 
---
+++ 6week_loopoperator.py
@@ -0,0 +1,42 @@
+
+# for item in range(2,10,2):
+#     print(item)
+
+# print(list(range(5,50,5)))
+
+# enumerate
+# index = 0
+greeting =  "Hello There"
+
+# for letter in greeting:
+#     print(f"index: {index} letter: {greeting[index]}")
+#     index += 1
+
+# for index, item in enumerate(greeting):
+#     print(index,item)
+
+# zip
+
+# l1 = [1,2,3,4,5]
+# l2 = ["a","b","c","d","e"]
+
+# l3 = list(zip(l1,l2))
+
+# print(l3[0][1])
+
+# comprehensions
+
+# for x in range(10):
+#     print(x)
+
+# numbers = [x for x in range(0,10,2)]
+# print(numbers)
+
+
+
+
+
+
+
+
+
