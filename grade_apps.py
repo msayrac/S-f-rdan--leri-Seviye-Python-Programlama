ee660782ce8a6066bcabb755d7875a8c8ce293bb
msayracode <msayracode@gmail.com>
2025-09-26 16:14:39 +0300

    note apps is updated

--- grade_apps.py
+++ grade_apps.py
@@ -1,6 +1,43 @@
 
+def not_hesapla(satir):
+    satir = satir[:-1]
+    liste = satir.split(":")
+    ogrenci_adi = liste[0]
+    print("Öğrenci Adı : ", ogrenci_adi)
+    notlar = liste[1].split(",")    
+
+    not1 = int(notlar[0])
+    not2 = int(notlar[1])
+    not3 = int(notlar[2])
+
+    ortalama = (not1 + not2 + not3)/3
+
+    if ortalama >= 90 and ortalama <=100:
+        harf = "AA"
+    elif ortalama >= 85 and ortalama <=89:
+        harf = "BA"
+    elif ortalama >= 80 and ortalama <=84:
+        harf = "BB"
+    elif ortalama >= 75 and ortalama <=79:
+        harf = "CB"
+    elif ortalama >= 70 and ortalama <=74:
+        harf = "CC"
+    elif ortalama >= 65 and ortalama <=69:
+        harf = "DC"
+    elif ortalama >= 60 and ortalama <=64:
+        harf = "DD"
+    elif ortalama >= 50 and ortalama <=59:
+        harf = "FD"    
+    else:
+        harf = "FF"
+    
+    return ogrenci_adi + " : " + harf + "\n"
+
+
 def ortalamalari_oku():
-    pass
+    with open("sinav_notlari.txt","r",encoding="utf-8") as file:
+        for satir in file:
+            print(not_hesapla(satir))
 
 def not_gir():
     ad = input("Ögrenci Adı :")
@@ -10,10 +47,21 @@ def not_gir():
     not3 = input("Not 3 : ")
 
     with open("sinav_notlari.txt","a",encoding="utf-8") as file:
-        file.write(ad+ " "+ soyad + ":"+ not1+ ", ", not2+ ", "+ not3+"\n")
+        file.write(ad + " " + soyad + ":"+ not1 + ", " + not2+ ", "+ not3+ "\n")
 
 def notlari_kayitet():
-    pass
+    with open("sinav_notlari.txt","r", encoding="utf-8") as file:
+        liste = []
+
+        for i in file:
+            liste.append(not_hesapla(i))
+        
+        with open("sonuclar.txt","w", encoding = "utf-8") as file2:
+            for i in liste:
+                file2.write(i)
+
+        
+
 
 
 
