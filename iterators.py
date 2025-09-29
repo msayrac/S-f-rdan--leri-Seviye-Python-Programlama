0c1b66499e28f42a64393ef8076f30682293d8bb
msayracode <msayracode@gmail.com>
2025-09-29 09:49:24 +0300

    generator yield taleb ederiz tek bir defa kullanmak istersen o zmaan generator kullanırız

--- iterators.py
+++ iterators.py
@@ -1,37 +1,51 @@
 
-# liste = [1,2,3,4,5]
+# # liste = [1,2,3,4,5]
 
-# # iterators
+# # # iterators
 
-# iterator = iter(liste)
-
-
-# while True:
-#     try:
-#         element = next(iterator)
-#         print(element)
-#     except Exception as ex:
-#         # print("bitti",ex)
-#         break
+# # iterator = iter(liste)
 
 
-class MyNumber:
-    def __init__(self,start, stop):
-        self.start = start
-        self.stop = stop
+# # while True:
+# #     try:
+# #         element = next(iterator)
+# #         print(element)
+# #     except Exception as ex:
+# #         # print("bitti",ex)
+# #         break
 
-    def __iter__(self):
-        return self
 
-    def __next__(self):
-        if self.start <= self.stop:
-            x = self.start
-            self.start += 1
-            return x
-        else:
-            raise StopIteration
+# class MyNumber:
+#     def __init__(self,start, stop):
+#         self.start = start
+#         self.stop = stop
 
-list = MyNumber(10,20)
+#     def __iter__(self):
+#         return self
 
-for x in list:
-    print(x)
+#     def __next__(self):
+#         if self.start <= self.stop:
+#             x = self.start
+#             self.start += 1
+#             return x
+#         else:
+#             raise StopIteration
+
+# list = MyNumber(10,20)
+
+# for x in list:
+#     print(x)
+
+# generators
+
+def cube():
+
+    for i in range(5):
+        yield i ** 3
+        # return i ** 2
+
+generator = cube()
+
+iterator = iter(generator)
+print(next(iterator))
+print(next(iterator))
