46ac5f38fbdda667a001323e3f27f16a3bf63d64
msayracode <msayracode@gmail.com>
2025-10-02 10:04:41 +0300

    os module

---
+++ 16week_modules.py
@@ -0,0 +1,38 @@
+
+import os
+# işletim sistemi ile ilgili bilgi sunuar bilgi talebi 
+
+result = dir(os)
+result = os.name
+
+result = os.getcwd()
+
+result = os.listdir()
+
+# result = os.listdir("C://")
+
+# for dosya in os.listdir():
+#     if dosya.endswith(".py"):
+#         print(dosya)
+
+result = os.stat("")
+
+print(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
